9ca79c76891c40609d772d6067afbd3981e68d50
Anton Konkin <anton.konkin@medtronic.com>
2021-01-28 16:04:34 +0200

    Add db remove mock repo

---
+++ Contr/CommandsController.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Contr
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly  ICommanderRepo _repository;
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+        
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandsItems = _repository.GetAllCommands();
+            return Ok(commandsItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
